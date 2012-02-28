aabc7aeaab4c50f2e66d090ba371174040a40cc4
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2012-02-28 12:36:17 +0200

    API Summary

--- pages/api/menu.php
+++ pages/api/menu.php
@@ -4,6 +4,14 @@ Layout_Menu::render('API', array(
   'Introduction' => array(
     'Making Requests' => '/api/introduction/requests',
   ),
+  'API' => array(
+    'Client' => '/api/api#client',
+    'Batch' => '/api/api#batch',
+  ),
+  'Other' => array(
+    'Home' => '/home',
+    'Blog' => '/blog',
+  ),
 ));
 
 Layout_Page::content();
