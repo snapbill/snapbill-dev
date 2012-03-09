4786947e2145380c4f122f150cb513664591c16d
Josh Yudaken <j@yud.co.za>
2012-03-09 10:49:08 +0200

    Add menu for the 404 page

---
+++ pages/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+Layout_Menu::setup('Home', array(
+  'Concepts' => array(
+    'Actions' => '/home/concepts/actions',
+    'Permissions' => '/home/concepts/permissions',
+    'Xid' => '/home/concepts/xid',
+  ),
+  'Templates' => array(
+    'Reference' => '/home/templates/reference',
+    'Files' => '/home/templates/files',
+  ),
+));
+
+Layout_Page::content();
