a3bf9b636e883c9851a5e68fc21ec7efce0267b0
Josh Yudaken <j@yud.co.za>
2012-03-11 10:23:43 +0200

    Add a signup concept page

--- pages/home/menu.php
+++ pages/home/menu.php
@@ -4,6 +4,7 @@ Layout_Menu::setup('Home', array(
   'Concepts' => array(
     'Actions' => '/home/concepts/actions',
     'Permissions' => '/home/concepts/permissions',
+    'Signup' => '/home/concepts/signup',
     'Xid' => '/home/concepts/xid',
   ),
   'Templates' => array(
