4baf92d5e6ea40212cb1f2af0585f72783aa5b9c
Josh Yudaken <j@yud.co.za>
2012-03-12 16:04:28 +0200

    Added signatures page to home section

--- pages/home/menu.php
+++ pages/home/menu.php
@@ -4,6 +4,7 @@ Layout_Menu::setup('Home', array(
   'Concepts' => array(
     'Actions' => '/home/concepts/actions',
     'Permissions' => '/home/concepts/permissions',
+    'Signatures' => '/home/concepts/signatures',
     'Signup' => '/home/concepts/signup',
     'Xid' => '/home/concepts/xid',
   ),
